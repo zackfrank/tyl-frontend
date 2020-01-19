0500515233eeb83f46d104b107cc8f788fce05cb
Zack Frank <frank.zack@gmail.com>
2020-01-19 02:54:49 -0500

    Begin styling cards

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -22,18 +22,37 @@ export default {
 
 <template>
   <section>
-    <div v-for="card in selectedCards" :key="card.id">
+    <div 
+      v-for="card in selectedCards"
+      :key="card.id"
+      class="card"
+    >
       {{ card.title }}
     </div>
     <Card/>
   </section>
 </template>
 
-<style scoped>
+<style lang=scss scoped>
 section {
   float: right;
   display: inline-block;
   margin-top: 15px;
   width: 81%;
+}
+
+.card {
+  width: 200px;
+  padding: 15px 15px 15px 15px;
+  margin-top: 6px;
+  margin-right: 4px;
+  background: #EEEEEE;
+  box-shadow: 2px 2px 1px 0px rgba(0, 0, 0, 0.15);
+  opacity: 0.9;
+  cursor: pointer;
+  border-radius: 7px;
+  &:hover {
+    opacity: 1;
+  };
 }
 </style>
